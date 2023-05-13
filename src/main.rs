5592c265862d04f2fa0c82d90c7ac1289beb1bf5
pklvz <pklvz@outlook.com>
2023-05-13 16:46:46 +0800

    Do not allow to skip type check

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
 use anyhow::Result;
 use clap::Parser;
 use std::fs;
-use tiger::{eval::eval, parser::parse, types::check};
+use tiger::{eval, parser, types};
 
 /// Tiger language interpreter
 #[derive(Parser, Debug)]
 struct Args {
-    /// Skip type check
-    #[arg(short, long)]
-    skip_type_check: bool,
-
     file_name: String,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
     let src = fs::read_to_string(args.file_name)?;
-    let ast = parse(&src)?;
-    if !args.skip_type_check {
-        check(&ast)?;
-    }
-    eval(&ast)?;
+    let ast = parser::parse(&src)?;
+    types::check(&ast)?;
+    eval::eval(&ast)?;
     Ok(())
 }
